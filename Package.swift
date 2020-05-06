f64a8b034dcaee1481e75ff04d0f0358f4520609
psksvp <psksvp@gmail.com>
2020-05-06 14:57:42 +1000

    check with CommonSwift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// psksvp@gmail.com
+
 import PackageDescription
 
 let package = Package(
